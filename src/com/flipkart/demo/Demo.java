6bf84a0c36aff26e9b908ebcf88f75d2977b6720
Ishika Gupta <ishika.gupta@BLRETV-2F5701MD.local>
2023-05-17 17:31:57 +0530

    First Commit

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author ishika.gupta
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("THIS IS MY FIRST PROGRAM");
+	}
+
+}
